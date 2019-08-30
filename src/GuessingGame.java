f2dbc527da9d57d7a9a5b02d01612aa6e8f22fee
Navya Kaliki <Navya.Kaliki@myob.com>
2019-08-30 12:07:39 +1000

    Game with hints, couting tries and also ignoring repeated numbers

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GuessingGame {
@@ -5,28 +6,32 @@ public class GuessingGame {
         double secretNum = Math.random();
         double rounded = Math.ceil(secretNum * 10);
         Scanner scanner = new Scanner(System.in);
-        int count=0;
-          while(true) {
-           System.out.println("Enter your guessing ");
-           int userInput = Integer.parseInt(scanner.nextLine());
-           if (rounded == userInput)
-           {
-               System.out.println("You guessed it right");
-               System.out.printf("number of tries %d\n",count);
-               System.exit(0);
-           }
-           else
-           {
-               if (rounded > userInput) {
-                   System.out.println("Guess is Small");
-               } else if (rounded < userInput) {
-                   System.out.println("Guess is big");
-               }
-           }
-           count=count+1;
-           System.out.println("Try again");
-       }
-
+        int count = 1;
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        while (true) {
+            System.out.println("Enter your guessing ");
+            int userInput = Integer.parseInt(scanner.nextLine());
+            if (al.contains(userInput)) {
+                System.out.println("guessed already");
+            } else {
+                al.add(userInput);
+                System.out.println(al);
+                {
+                    if (rounded == userInput) {
+                        System.out.println("You guessed it right");
+                        System.out.printf("number of tries %d\n", count);
+                        System.exit(0);
+                    } else {
+                        if (rounded > userInput) {
+                            System.out.println("Guess is Small");
+                        } else if (rounded < userInput) {
+                            System.out.println("Guess is big");
+                        }
+                        count = count + 1;
+                        System.out.println("Try again");
+                    }
+                }
+            }
+        }
     }
 }
-
